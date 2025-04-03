5569bfe7c5b39157a54eb3c929326452b0ee1866
Anusha Gajanan Bhat <anushagajananbhat2002@gmail.com>
2025-04-03 20:48:56 +0530

    ChromeDB integration

---
+++ utils/vector_search.py
@@ -0,0 +1,22 @@
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import chromadb
+
+def build_faiss_index(df, model):
+    text_data = df['hotel'].astype(str) + " " + df['customer_type'].astype(str)
+    embeddings = model.encode(text_data.to_list())
+    
+    index = faiss.IndexFlatL2(embeddings.shape[1])
+    index.add(np.array(embeddings, dtype=np.float32))
+    
+    return index, text_data.to_list()
+
+def load_chroma_db(metadata):
+    chroma_client = chromadb.PersistentClient(path="chroma_db")
+    collection = chroma_client.get_or_create_collection(name="bookings")
+    
+    for i, text in enumerate(metadata):
+        collection.add(ids=[str(i)], documents=[text])
+    
+    return collection
