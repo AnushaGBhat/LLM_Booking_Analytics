8e97d5b6774865703b614b67acb65bf444698481
Anusha Gajanan Bhat <anushagajananbhat2002@gmail.com>
2025-04-03 20:53:09 +0530

    Updated ChromaDB

--- utils/vector_search.py
+++ utils/vector_search.py
@@ -4,19 +4,19 @@ from sentence_transformers import SentenceTransformer
 import chromadb
 
 def build_faiss_index(df, model):
-    text_data = df['hotel'].astype(str) + " " + df['customer_type'].astype(str)
+    text_data = df['hotel'].astype(str) + " " + df['customer_type'].astype(str) + " " + df['reservation_status'].astype(str)
     embeddings = model.encode(text_data.to_list())
-    
+
     index = faiss.IndexFlatL2(embeddings.shape[1])
     index.add(np.array(embeddings, dtype=np.float32))
-    
+
     return index, text_data.to_list()
 
 def load_chroma_db(metadata):
     chroma_client = chromadb.PersistentClient(path="chroma_db")
     collection = chroma_client.get_or_create_collection(name="bookings")
-    
+
     for i, text in enumerate(metadata):
         collection.add(ids=[str(i)], documents=[text])
-    
+
     return collection
